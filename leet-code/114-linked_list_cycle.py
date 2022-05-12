be3e4d6cffa20cb583bdcd2a645978b312fdf791
Hoai An Phan <pnhoaian2904@gmail.com>
2022-05-12 04:49:21 +0000

    Added unit tests

--- leet-code/114-linked_list_cycle.py
+++ leet-code/114-linked_list_cycle.py
@@ -50,3 +50,65 @@ def has_cycle(head):
 #Runtime complexity: O(n)
 
 
+class MyTestCase(unittest.TestCase):
+    """ Unit test """
+    def test_llist_cycle(self):
+        """ 3 -> 2 -> 0 -> -4 
+                 ^          |
+                 |----------| """
+        
+        node1 = ListNode("3")
+        node2 = ListNode("2")
+        node3 = ListNode("0")
+        node4 = ListNode("-4")
+        node1.next = node2
+        node2.next = node3
+        node3.next = node4
+        node4.next = node2
+
+        self.assertIs(has_cycle(node1), True)
+
+    def test_empty_llist(self):
+        """ Empty linked list """
+        node1 = None
+        self.assertIs(has_cycle(node1), False)
+
+    def test_one_node(self):
+        """ 1 """
+
+        node1 = ListNode("1")
+
+        self.assertIs(has_cycle(node1), False)
+
+    def test_no_cycle(self):
+        """ 3 -> 2 -> 0 -> -4 """
+        node1 = ListNode("3")
+        node2 = ListNode("2")
+        node3 = ListNode("0")
+        node4 = ListNode("-4")
+        node1.next = node2
+        node2.next = node3
+        node3.next = node4
+
+        self.assertIs(has_cycle(node1), False)
+
+    def test_no_cycle_repeating_value(self):
+        """ 3 -> 2 -> 0 -> -4 -> 2 -> 1"""
+        node1 = ListNode("3")
+        node2 = ListNode("2")
+        node3 = ListNode("0")
+        node4 = ListNode("-4")
+        node5 = ListNode("2")
+        node6 = ListNode("1")
+        node1.next = node2
+        node2.next = node3
+        node3.next = node4
+        node4.next = node5
+        node5.next = node6
+
+        self.assertIs(has_cycle(node1), False)
+
+testclass = unittest.main(exit=False)
+
+if testclass.result.wasSuccessful():
+    print("All tests pass -- Nice work!")
