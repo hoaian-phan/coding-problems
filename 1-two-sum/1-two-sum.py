1e1b5b3cd025bea529561bf16e714c2461920f6f
Hoai-An Phan <98496276+hoaian-phan@users.noreply.github.com>
2022-06-03 12:35:00 -0700

    Update 1-two-sum.py

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -7,8 +7,8 @@ class Solution:
             # Runtime: O(n^2)
             # for i in range(len(nums) - 1):
             #     for j in range(i+1, len(nums)):
-            #     if nums[i] + nums[j] == target:
-            #         return [i,j]
+            #         if nums[i] + nums[j] == target:
+            #             return [i,j]
                 
         # Optimized approach:
             # Create a dictionary with keys being the numbers and values being the 
