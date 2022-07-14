e0911c3ae252edf07ac456f9adb1ae01bc76356e
Hoai-An Phan <98496276+hoaian-phan@users.noreply.github.com>
2022-07-14 15:45:32 -0700

    Time: 507 ms (80.75%), Space: 26 MB (72.31%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.py
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -1,35 +1,8 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        # Brute-force:
-        # Edge cases:
-        # empty list -> False
-        # one element list -> False
-        # Sort the list in ascending order so that the same values will be next to one another
-        # Iterate through the list, compare the two adjacent values
-        # If they are equal -> return True
-        # If not, keep iterating
-        # Done iteration, return False
+        # make a unique set of nums and compare the length of the set vs the list
         
-#         if len(nums) < 2:
-#             return False
+        num_set = set(nums)
         
-#         nums.sort()
+        return len(num_set) != len(nums)
         
-#         for index in range(len(nums) - 1):
-#             if nums[index] == nums[index + 1]:
-#                 return True
-            
-#         return False
-
-
-        # Optimize:
-            # Create a set of nums
-            # Compare size of set and list
-            # If same -> no duplicate -> return False
-            # else -> return True
-            
-            nums_set = set(nums)
-            if len(nums_set) == len(nums):
-                return False
-            
-            return True
