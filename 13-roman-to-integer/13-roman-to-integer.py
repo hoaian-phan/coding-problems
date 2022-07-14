f5fa2d6ec8b71e4e0dc9760c47afa1be24224155
Hoai-An Phan <98496276+hoaian-phan@users.noreply.github.com>
2022-07-13 22:52:15 -0700

    Time: 70 ms (60.20%), Space: 13.9 MB (76.17%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.py
+++ 13-roman-to-integer/13-roman-to-integer.py
@@ -1,55 +1,41 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        # Make a dictionary to store roman letter: integer as key: value
-        # Create a num integer = 0
-        # Iterate through the input string s, checking each char
-            # If char is I and not at end of string, check next char
-                # If next char is V -> add 5 to num
-                # If next char is X -> add 9 to num
-            # If char is X and not at end of string, check next char
-                # If next char is L -> add 50 to num
-                # If next char is C -> add 90 to num
-            # If char is C and not at end of string, check next char
-                # If next char is D -> add 500 to num
-                # If next char is M -> add 900 to num
-        # Return num
+        # create a result variable
+        # make a dict {roman:numeral} including all special cases
+        # iterate through the string
+            # check if IX -> add dict[IX]
+            # if IV -> add dict[IV]
+            # ...
+            # else, add dict[element]
+            
+            
+        result = 0
         
-        roman_int = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-        
-        num = 0
+        roman_dict = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000,
+            "IV": 4,
+            "IX": 9,
+            "XL": 40,
+            "XC": 90,
+            "CD": 400,
+            "CM": 900,
+        }
         
         i = 0
-        while i < len(s):
-            if i < len(s) - 1 and s[i] == "I":
-                if s[i+1] == "V":
-                    num += 4
-                    i += 1
-                elif s[i+1] == "X":
-                    num += 9
-                    i += 1
-                else:
-                    num += 1
-                
-            elif i < len(s) - 1 and s[i] == "X":
-                if s[i+1] == "L":
-                    num += 40
-                    i += 1
-                elif s[i+1] == "C":
-                    num += 90
-                    i += 1
-                else:
-                    num += 10
-            elif i < len(s) - 1 and s[i] == "C":
-                if s[i+1] == "D":
-                    num += 400
-                    i += 1
-                elif s[i+1] == "M":
-                    num += 900
-                    i += 1
-                else:
-                    num += 100
+        while i < len(s) - 1:
+            if s[i] == "I" and s[i+1] in ["V", "X"] or s[i] == "X" and s[i+1] in ["L", "C"]  or s[i] == "C" and s[i+1] in ["D", "M"]:
+                result += roman_dict[s[i]+s[i+1]] 
+                i += 2
             else:
-                num += roman_int[s[i]]
-            i += 1
+                result += roman_dict[s[i]]
+                i += 1
                 
-        return num
+        return result if i >= len(s) else result + roman_dict[s[i]]
+        
+        
