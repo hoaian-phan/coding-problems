564220ba80578cc702cd4c6555e2c1692857f3ef
Hoai-An Phan <98496276+hoaian-phan@users.noreply.github.com>
2022-07-15 16:11:58 -0700

    Time: 632 ms (97.91%), Space: 18.1 MB (72.14%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -1,32 +1,38 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        # brute force
+        # sort the list to avoid duplicate
+        # looping through the list forr the left number
+        # use two pointers for mid and right index similarly as in two sum problem
         
-        results = []
         
         nums.sort()
         
+        results = []
+        
         for left in range(len(nums) - 2):
+            # if the first number is positive, can't find the other 2 positive nums that sum up to 0
             if nums[left] > 0:
                 break
-            elif left > 0 and nums[left] == nums[left - 1]:
+            # avoid duplicate for first num
+            elif left > 0 and nums[left] == nums[left-1]:
                 continue
             
+            # use two pointers as in two sum
             mid = left + 1
             right = len(nums) - 1
             while mid < right:
-                sum_triplets = nums[left] + nums[mid] + nums[right] 
-                if sum_triplets < 0:
+                three_sum = nums[left] + nums[mid] + nums[right]
+                if three_sum < 0:
                     mid += 1
-                elif sum_triplets > 0:
+                elif three_sum > 0: 
                     right -= 1
                 else:
-                    results.append([nums[left], nums[mid], nums[right]])
+                    results.append([nums[left],nums[mid],nums[right]])
+                    # keep looking for more values of 2nd and 3rd num
                     mid += 1
+                    # avoid duplicate for 2nd num
                     while nums[mid] == nums[mid-1] and mid < right:
                         mid += 1
-            
-            
-            
-        return results
         
+        return results
+            
