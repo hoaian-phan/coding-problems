b0f5bfd76a7e95c60e850a435c43f2df00b4a27d
Hoai-An Phan <98496276+hoaian-phan@users.noreply.github.com>
2022-07-15 12:11:51 -0700

    Time: 2804 ms (22.89%), Space: 29 MB (20.39%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.py
@@ -9,43 +9,14 @@ class Solution:
             
         num_set = set(nums)
         longest = 0
-        count = 0
         
         for num in nums:
-            if num -1 not in num_set:
+            if (num-1) not in num_set:
                 count = 1
-                while num + count in num_set:
+                while (num + count) in num_set:
                     count += 1
                     
                 longest = max(longest, count)
             
         return longest
-        
-        
-        
-        
-        
-        # Brute force: sort the list
-        # create longest = 0
-        # create count to keep track
-        # iterate through the sorted list, compare two elements
-        # if difference is 1 -> increment count, calculate longest = max(count, longest)
-        #runtime: O (n log n)
-        
-#         if len(nums) < 2:
-#             return len(nums)
-        
-#         nums.sort()
-#         longest = 0
-#         count = 1
-        
-#         for i in range(len(nums)-1):
-#             if nums[i+1] == nums[i]:
-#                 continue
-#             elif nums[i+1] - nums[i] == 1:
-#                 count += 1
-#                 longest = max(count, longest)
-#             else:
-#                 count = 1
-                
-#         return max(longest, count)
+      
