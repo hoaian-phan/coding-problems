c2e32a2f2b544f7cdd0db8f0060087f44eb2ec9d
Hoai-An Phan <98496276+hoaian-phan@users.noreply.github.com>
2022-07-17 00:09:00 -0700

    Time: 60 ms (61.75%), Space: 16.7 MB (5.93%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.py
+++ 205-isomorphic-strings/205-isomorphic-strings.py
@@ -1,40 +1,18 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
+        # create dictionaries {char:[indices]}
+        # compares values of 2 dictionaries
         
-        # if length of s and t are not the same -> false
-        # if two strings are the same -> true
-        # Make 2 dicts {char:[index,index,index]}
-        # Compare the values of the two dicts
-            # same -> True
-            # not -> False
-            
-        if len(s) != len(t):
-            return False
+        s_dict = defaultdict(list)
+        for index, char in enumerate(s):
+            s_dict[char].append(index)
         
-        if s == t:
-            return True
+        t_dict = defaultdict(list)
+        for index, char in enumerate(t):
+            t_dict[char].append(index)
         
-        dict1 = {}
-        dict2 = {}
-        
-#         for char in s:
-#             dict1[char] = dict1.get(char, 0) + 1
-            
-#         for char in t:
-#             dict2[char] = dict2.get(char, 0) + 1
-            
-        for i in range(len(s)):
-            if s[i] not in dict1:
-                dict1[s[i]] = [i]
-            else:
-                dict1[s[i]].append(i)
-            if t[i] not in dict2:
-                dict2[t[i]] = [i]
-            else:
-                dict2[t[i]].append(i)
-        
-        for value in dict1.values():
-            if value not in dict2.values():
+        for value in s_dict.values():
+            if value not in t_dict.values():
                 return False
-                
+            
         return True
