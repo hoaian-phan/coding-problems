d1c70b4ba47275a15c6524b7199cf49ea24ec974
Hoai-An Phan <98496276+hoaian-phan@users.noreply.github.com>
2022-07-16 23:52:31 -0700

    Time: 68 ms (26.50%), Space: 13.9 MB (37.73%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.py
+++ 88-merge-sorted-array/88-merge-sorted-array.py
@@ -3,24 +3,30 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
+        # compare the largest elements and fill up nums1 from the back
         
-        # Fill up nums1 from the end onward, putting the larger value of nums1 vs nums2
         
-        last = len(nums1) - 1
+        i = m - 1
+        j = n - 1
+        k = len(nums1) - 1
         
-        while m > 0 and n > 0:
-            if nums1[m-1] >= nums2[n-1]:
-                nums1[last] = nums1[m-1]
-                m -= 1
+        while i >= 0 and j >= 0:
+            if nums1[i] >= nums2[j]:
+                nums1[k] = nums1[i]
+                i -= 1
             else:
-                nums1[last] = nums2[n-1]
-                n -= 1
-            last -= 1
+                nums1[k] = nums2[j]
+                j -= 1
+            k -= 1
             
-        while n > 0:
-            nums1[last] = nums2[n-1]
-            n -= 1
-            last -= 1
-        
+        while i >= 0:
+            nums1[k] = nums1[i]
+            i -= 1
+            k -= 1
+            
+        while j >= 0:
+            nums1[k] = nums2[j]
+            j -= 1
+            k -= 1
+            
         return nums1
-            
