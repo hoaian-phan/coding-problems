9ba79958e51b079de6291e112ee52418f10dc01e
Hoai-An Phan <98496276+hoaian-phan@users.noreply.github.com>
2022-07-17 23:00:23 -0700

    Time: 90 ms (32.71%), Space: 14.1 MB (51.99%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.py
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.py
@@ -1,56 +1,56 @@
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
         # if arrays are sorted, use 2 pointers technique
-        nums1.sort()
-        nums2.sort()
-        
-        results = []
-        
-        i = 0
-        j = 0
-        
-        while i < len(nums1) and j < len(nums2):
-            if nums1[i] < nums2[j]:
-                i += 1
-            elif nums1[i] > nums2[j]:
-                j += 1
-            else:
-                results.append(nums1[i])
-                i += 1
-                j += 1
-                
-        return results
-                
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        # brute force: make 2 dictionaries {num:count}
-        # iterate through keys of dict_1 and check if they are in dict_2
-        # if yes, add key to result list how many times depending on the min of the two values
-        # O(n^2)
+        # runtime: O(n log n + m log m)
+#         nums1.sort()
+#         nums2.sort()
         
 #         results = []
         
-#         dict_1 = {}
-#         for num in nums1:
-#             dict_1[num] = dict_1.get(num, 0) + 1
-            
-#         dict_2 = {}
-#         for num in nums2:
-#             dict_2[num] = dict_2.get(num, 0) + 1
-            
-#         for key, value in dict_1.items():
-#             if key in dict_2:
-#                 min_value = min(value, dict_2[key])
-#                 for n in range(min_value):
-#                     results.append(key)
-                    
+#         i = 0
+#         j = 0
+        
+#         while i < len(nums1) and j < len(nums2):
+#             if nums1[i] < nums2[j]:
+#                 i += 1
+#             elif nums1[i] > nums2[j]:
+#                 j += 1
+#             else:
+#                 results.append(nums1[i])
+#                 i += 1
+#                 j += 1
+                
 #         return results
+                
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # brute force: make 1 dictionary {num:count}
+        # iterate through the other list, check if element is in dictionary
+        # if yes, add key to result list how many times depending on the min of the two values
+        # O(n * m)
+        
+        results = []
+        
+        dict_1 = {}
+        for num in nums1:
+            dict_1[num] = dict_1.get(num, 0) + 1
+            
+        for num in nums2:
+            if num in dict_1 and dict_1[num] > 0:
+                results.append(num)
+                dict_1[num] -= 1
+                print(dict_1[num])
+        print(dict_1)
+                
+            
+                    
+        return results
                 
