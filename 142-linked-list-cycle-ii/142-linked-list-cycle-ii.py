8a56de6ff3ed783604435c6fffec9599f87e2280
Hoai-An Phan <98496276+hoaian-phan@users.noreply.github.com>
2022-07-18 22:24:11 -0700

    Time: 53 ms (92.51%), Space: 18 MB (10.23%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.py
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.py
@@ -6,17 +6,24 @@
 
 class Solution:
     def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        # keep track of traversed node in a set
-        # check each key to see if it is in the set -> return value of it
+        # if llist is empty or has only 1 node return None
+        # use current pointer to traverse through the llist, keep track of visited nodes in a set data structure
+        # if this node is already seen in the visited node set -> it's the start of the cycle
+        
+        if not head or not head.next:
+            return None
         
         current = head
-        node_set = set()
+        visited_nodes = set()
+        visited_nodes.add(head)
         
-        while current:
-            if current not in node_set:
-                node_set.add(current)
+        while current.next:
+            if current.next not in visited_nodes:
+                visited_nodes.add(current.next)
+                current = current.next
             else:
-                return current
-            current = current.next
+                return current.next
+            
             
         return None
+        
