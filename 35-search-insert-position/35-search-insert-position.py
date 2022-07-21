c8aa4f291389f0a58395d6b98da1d6dc7b608a55
Hoai-An Phan <98496276+hoaian-phan@users.noreply.github.com>
2022-07-21 16:34:59 -0700

    Time: 62 ms (74.48%), Space: 14.8 MB (41.01%) - LeetHub

---
+++ 35-search-insert-position/35-search-insert-position.py
@@ -0,0 +1,25 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        # use binary search algorithm
+        
+        
+        start = 0
+        end = len(nums) - 1
+        
+        # if nums[start] >= target:
+        #     return start
+        # elif nums[end] < target:
+        #     return end + 1
+        
+        while start < end:
+            mid = (start + end) // 2
+            if nums[mid] < target:
+                start = mid + 1
+            elif nums[mid] > target:
+                end = mid - 1
+            else: 
+                return mid
+            
+       
+        return start if nums[start] >= target else start + 1
+        
