96f9ea66af73ed45eab0b43767b878a7d320681c
Hoai-An Phan <98496276+hoaian-phan@users.noreply.github.com>
2022-07-22 11:09:58 -0700

    Time: 69 ms (17.13%), Space: 15 MB (8.91%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.py
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.py
@@ -6,27 +6,52 @@
 #         self.right = right
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        # iteratively using queue
-        
-        from collections import deque
-        
+        # recursively
         levels = []
         
         if not root: return levels
         
-        
-        queue = deque([root])
-        
-        while queue:
-            level = []
-            for n in range(len(queue)):
-                node = queue.popleft()
-                level.append(node.val)
-                if node.left:
-                    queue.append(node.left)
-                if node.right:
-                    queue.append(node.right)
-            levels.append(level)
+        def bfs(node, level=0):
+            if len(levels) == level:
+                levels.append([])
+                
+            levels[level].append(node.val)
+            
+            if node.left:
+                bfs(node.left, level + 1)
+            if node.right:
+                bfs(node.right, level + 1)
+                
+        bfs(root, 0)
         
         return levels
+        
+        
+        
+        
+        
+        
+        # iteratively using queue
+        
+#         from collections import deque
+        
+#         levels = []
+        
+#         if not root: return levels
+        
+        
+#         queue = deque([root])
+        
+#         while queue:
+#             level = []
+#             for n in range(len(queue)):
+#                 node = queue.popleft()
+#                 level.append(node.val)
+#                 if node.left:
+#                     queue.append(node.left)
+#                 if node.right:
+#                     queue.append(node.right)
+#             levels.append(level)
+        
+#         return levels
 #         
