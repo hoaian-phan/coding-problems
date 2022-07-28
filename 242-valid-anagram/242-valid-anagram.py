81e6316c98502bb45359c880bb6a4c5f581e6856
Hoai-An Phan <98496276+hoaian-phan@users.noreply.github.com>
2022-07-28 00:47:38 -0700

    Time: 52 ms (89.25%), Space: 14.5 MB (34.60%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,21 +1,7 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        # use one dictionary for s, and iterate through t to check for existence
-        if len(s) != len(t):
-            return False
         
-        s_dict = {}
-        for char in s:
-            s_dict[char] = s_dict.get(char, 0) + 1
+        
+        
+        return collections.Counter(s) == collections.Counter(t)
             
-        for letter in t:
-            if letter not in s_dict:
-                return False
-            elif s_dict[letter] == 0:
-                return False
-            else:
-                s_dict[letter] -= 1
-                
-        return True
-        
-        
