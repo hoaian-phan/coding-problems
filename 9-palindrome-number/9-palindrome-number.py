a9b24ae54bf0f0491c995d3c1f3a4f1c25090b06
Hoai-An Phan <98496276+hoaian-phan@users.noreply.github.com>
2022-07-29 10:30:27 -0700

    Time: 151 ms (7.88%), Space: 13.9 MB (58.82%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.py
+++ 9-palindrome-number/9-palindrome-number.py
@@ -1,19 +1,12 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        # Convert the number to string
-        # Iterate through the string to the middle
-        # compare the first and last char
-        # if not the same -> not palindrome
-        # end of loops, return True
+        # convert to string
+        # check for palindrome as usual
         
-        x_string = str(x)
-        print(x_string)
         
-        i = 0
-        while i < len(x_string) // 2:
-            if x_string[i] != x_string[len(x_string) - 1 - i]:
+        string = str(x)
+        for i in range(len(string)//2):
+            if string[i] != string[len(string)-1-i]:
                 return False
-            i += 1
             
         return True
-        
